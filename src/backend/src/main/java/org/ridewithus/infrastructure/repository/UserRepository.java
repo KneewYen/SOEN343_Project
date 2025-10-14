c62a4e9221baaa5be48c14fd65a0c3d4c752c7e9
JonathanFrantzeskos <jonathan2.frantzeskos@gmail.com>
2025-10-13 22:26:32 -0400

    user db set up

---
+++ src/backend/src/main/java/org/ridewithus/infrastructure/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package org.ridewithus.infrastructure.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.ridewithus.domain.entity.User;
+// import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;  //returns list of all users that follow the criteria 
+import java.util.Optional;  // returns Optional.of(user) if found. To get user, do Optional.get(). If no match : Optional.empty()
+
+@Repository
+public interface  UserRepository extends JpaRepository<User, Long> {
+    // Find user by email
+    Optional<User> findByEmail(String email);
+
+    // Check if email exists
+    boolean existsByEmail(String email);
+}
