f503ebd2bf4e7669797b7d0dea5caddd7758e432
NDC0DE <153046097+NDC0DE@users.noreply.github.com>
2025-11-20 18:32:35 -0500

    p4- Added Station Full event

--- src/backend/src/main/java/org/ridewithus/domain/services/TripService.java
+++ src/backend/src/main/java/org/ridewithus/domain/services/TripService.java
@@ -109,6 +109,7 @@ public class TripService {
 
         List<Dock> docks = dockRepository.findByStationAndStatus(station.get(), Dock.DockStatus.EMPTY);
 
+
         if (docks.isEmpty()) {
             throw new Exception("No empty places available to Dock");
         }
@@ -133,7 +134,7 @@ public class TripService {
         trip.setEndStation(station.get());
         trip.setTripComplete(true);
 
-        //Store user for event 
+        //Store user for event
         User user = trip.getReservation().getUser();
 
         // Store reservation reference before clearing it
@@ -149,6 +150,13 @@ public class TripService {
         String newStatus = reservation.getBike().getStatus().toString();
 
         eventService.emitEvent(user,"TRIP_ENDED", String.format("Trip %d ended - Bike %d: %s -> %s", trip.getTripId(), reservation.getBike().getId(), oldStatus, newStatus));
+
+        // Get free docks at destination
+        List<Dock> freeDocks = dockRepository.findAllByStationAndStatus(station.get(), Dock.DockStatus.EMPTY);
+
+        if(freeDocks.isEmpty()){
+            eventService.emitEvent(user, "STATION_FULL", String.format("%s station is full", station.get().getName()));
+        }
 
         return trip.getTripId();
 
